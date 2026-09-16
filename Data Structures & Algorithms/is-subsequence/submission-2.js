1066be6ff908fa84d807e11114af519f00d2124a
Sylwester Sobczak <78635631+zibi-dev@users.noreply.github.com>
2026-09-16 20:42:01 +0200

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        let i = 0;
+        let j = 0;
+
+        while (i < s.length && j < t.length) {
+            if (s[i] === t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return i === s.length;
+    }
+}
